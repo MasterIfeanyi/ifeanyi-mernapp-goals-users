29a02bf3d3464b62e2ae79efb916db877fe179ee
Ifeanyi <chimaifeanyi29@gmail.com>
2022-05-01 19:14:40 +0100

    prepare for deploy

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import User from "./User";
 import { useNavigate, useLocation } from "react-router-dom";
