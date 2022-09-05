b39b4939cb7f011939ff1e8226a73954ddb142a5
Ifeanyi <chimaifeanyi29@gmail.com>
2022-09-05 23:37:16 +0100

    change base URL"

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 // const BASE_URL = "http://localhost:3500"
-const BASE_URL = "https://ifeanyi-goals-users-backend.herokuapp.com/"
+// const BASE_URL = "https://ifeanyi-goals-users-backend.herokuapp.com/"
+const BASE_URL = "https://ifeanyi-goals-backend.glitch.me/"
 
 export default axios.create({
     baseURL: BASE_URL
